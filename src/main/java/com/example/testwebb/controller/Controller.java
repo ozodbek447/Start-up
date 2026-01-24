3810684cdf9aaa30c6c723038c18cd6033f52bc5
ozodbek447 <ozodozod181@gmail.com>
2026-01-24 12:34:03 +0500

    scond commit

--- src/main/java/com/example/testwebb/controller/Controller.java
+++ src/main/java/com/example/testwebb/controller/Controller.java
@@ -3,7 +3,7 @@ package com.example.testwebb.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@org.springframework.stereotype.Controller
+@Controller
 @RequestMapping
 public class Controller {
 
