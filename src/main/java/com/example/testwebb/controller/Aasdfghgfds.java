57512c903a1b8b4dba29f49f228b5ba816726cb6
ozodbek447 <ozodozod181@gmail.com>
2026-01-24 12:40:41 +0500

    aa commit

--- src/main/java/com/example/testwebb/controller/Aasdfghgfds.java
+++ src/main/java/com/example/testwebb/controller/Aasdfghgfds.java
@@ -1,14 +1,15 @@
 package com.example.testwebb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
-public class Controller {
 
+public class Aasdfghgfds {
     @GetMapping("/admin")
     public String admin() {
-        return "index";
+        return "main";
     }
 }
