b6004e86c566a16089e786ed0731cd13481a6762
ozodbek447 <ozodozod181@gmail.com>
2026-02-04 09:50:38 +0500

    aaa commit

--- src/main/java/com/example/testwebb/controller/Aasdfghgfds.java
+++ src/main/java/com/example/testwebb/controller/Aasdfghgfds.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Aasdfghgfds {
     @GetMapping("/admin")
     public String admin() {
-        return "main";
+        return "index";
     }
 }
